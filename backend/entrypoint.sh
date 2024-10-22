f5918da3de7b6d542ca04c5f9e202ed8da23b9cb
Lucas <lukas.figueiredo@hotmail.com>
2024-10-22 11:56:57 -0300

    Initial commit:   - Created a new Laravel 5.6 project   - Configured Docker with PHP, Timescaledb, and Redis services   - Added Dockerfile and docker-compose.yml for local development   - Set up necessary volumes and ports for the services

---
+++ backend/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Check if vendor directory exists
+if [ ! -d "vendor" ]; then
+  echo "Installing composer dependencies..."
+  composer install
+else
+  echo "Composer dependencies are already installed."
+fi
+
+# Copy .env.example to .env if .env does not exist
+if [ ! -f ".env" ]; then
+  echo "Copying .env.example to .env..."
+  cp .env.example .env
+  php artisan key:generate
+fi
+
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Run the main container command
+exec "$@"
