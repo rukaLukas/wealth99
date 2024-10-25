5b6a778d00d7144bcd0726fdbd8904132fffa897
Lucas <lukas.figueiredo@hotmail.com>
2024-10-25 00:21:23 -0300

    ..

--- backend/app/Http/Requests/DateTimeRequest.php
+++ backend/app/Http/Requests/DateTimeRequest.php
@@ -2,31 +2,32 @@
 namespace App\Http\Requests;
 
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Http\FormRequest;
 
 class DateTimeRequest extends FormRequest
 {
-    protected function prepareForValidation()
-    {
-        $this->merge(['datahora' => $this->datahora]); 
-    }
-
     public function rules()
     {       
         return [
-            'datahora' => 'required|date_format:Y-m-d H:i:s',
+            'datetimepath' => 'required|date_format:Y-m-d H:i:s',
         ];
     }
 
     public function messages()
     {
         return [
-            'datahora.date_format' => 'The datetime must be in the format: Y-m-d H:i:s',
+            'datetimepath.date_format' => 'The datetime must be in the format: Y-m-d H:i:s',
         ];
     }
 
     public function authorize()
     {
         return true;
-    }    
+    }  
+    
+    protected function validationData()
+    {
+        return $this->route()->parameters();
+    } 
 }
